eb002010a55f66f0a0f3c96fb61f41e5fc00c336
Adrian Weber <adrian.a.weber@physik.uni-giessen.de>
2018-10-19 11:13:44 +0200

    added missing download pathes

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ wget https://root.cern/js/latest/scripts/d3.min.js;
 wget https://root.cern/js/latest/scripts/JSRootPainter.js;
 wget https://root.cern/js/latest/scripts/JSRootPainter.v6.js;
 wget https://root.cern/js/latest/scripts/JSRootPainter.hist.js;
+wget https://root.cern/js/latest/scripts/jquery.min.js;
+wget https://root.cern/js/latest/scripts/jquery-ui.min.js;
+wget https://root.cern/js/latest/scripts/saveSvgAsPng.min.js;
 
 cd ../;
 mkdir style;
